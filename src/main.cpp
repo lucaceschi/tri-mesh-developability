fbafeaa7430f3db2cff30962061124a5c3f8f85a
Luca Franceschi <l.franceschi5@studenti.unipi.it>
2022-07-05 16:18:52 +0200

    fix #2 of gradient & ignoring border vertices

--- src/main.cpp
+++ src/main.cpp
@@ -44,7 +44,9 @@ double localCombinatorialEnergy(MyVertex* v,
         
     double energy = -1.0;
 
-    if(star.size() > 3)
+    if(star.size() <= 3 || v->IsB())
+        energy = 0.0;
+    else
     {
         // consider all possible cardinalities of a region resulting from a partitioning of the star
         for(int rSize = 2; rSize <= (star.size()-2); rSize++)
@@ -71,17 +73,6 @@ double localCombinatorialEnergy(MyVertex* v,
             }
         }
     }
-    else if(star.size() == 2)
-    {
-        energy = 0.0;
-        if(partitioning != nullptr)
-        {
-            partitioning->star = star;
-            partitioning->rBegin = 0;
-            partitioning->rSize = 1;
-        }
-    }
-    else return -1.0;
 
     return energy;
 }
@@ -107,7 +98,7 @@ void combinatorialEnergyGrad(MyMesh& mesh,
         triArea[fIter] = 0.0;
 
     auto faceNormalGrad = [&triArea](MyFace* f, int v) -> vcg::Matrix33d {
-        vcg::Point3d oppositeEdge = f->V1(v)->P() - f->V2(v)->P();
+        vcg::Point3d oppositeEdge = f->V2(v)->P() - f->V1(v)->P();
         vcg::Matrix33d grad;
 
         // if unknown, compute&store triangle area
@@ -143,14 +134,14 @@ void combinatorialEnergyGrad(MyMesh& mesh,
                         faceBv = faceB->V(faceBvIndex);
                         if(faceAv == faceBv)
                         {
-                            energyGrad[v] += ((faceNormalGrad(faceA, faceAvIndex) - faceNormalGrad(faceB, faceBvIndex)).transpose() * faceNormDiff * 2);
+                            energyGrad[faceAv] += ((faceNormalGrad(faceA, faceAvIndex) - faceNormalGrad(faceB, faceBvIndex)).transpose() * faceNormDiff * 2/std::pow(regionSize, 2));
                             sharedVertex = true;
                             break;
                         }
                     }
 
                     if(!sharedVertex)
-                        energyGrad[v] += (faceNormalGrad(faceA, faceAvIndex).transpose() * faceNormDiff * 2);
+                        energyGrad[faceAv] += (faceNormalGrad(faceA, faceAvIndex).transpose() * faceNormDiff * 2/std::pow(regionSize, 2));
                 }
 
                 for(faceBvIndex = 0; faceBvIndex < 3; faceBvIndex++)
@@ -170,7 +161,7 @@ void combinatorialEnergyGrad(MyMesh& mesh,
                     }
 
                     if(!sharedVertex)
-                        energyGrad[v] += (faceNormalGrad(faceB, faceBvIndex).transpose() * faceNormDiff * 2);
+                        energyGrad[faceBv] -= (faceNormalGrad(faceB, faceBvIndex).transpose() * faceNormDiff * 2/std::pow(regionSize, 2));
                 }
             }
     };
@@ -180,13 +171,11 @@ void combinatorialEnergyGrad(MyMesh& mesh,
         currEnergy = localCombinatorialEnergy(&*vIter, &currPart);
         localEnergyCallback(currEnergy, &*vIter);
 
-        if(currPart.star.size() == 3)
+        if(currPart.star.size() <= 3 || vIter->IsB())
             continue;
 
         regionNormalDeviationGrad(&*vIter, currPart.star, currPart.rBegin, currPart.rSize);
         regionNormalDeviationGrad(&*vIter, currPart.star, currPart.rBegin+currPart.rSize, currPart.star.size() - currPart.rSize);
-
-        energyGrad[vIter] /= std::pow(currPart.star.size(), 2);
     }
 }
 
@@ -199,6 +188,8 @@ int main(int argc, char* argv[])
         return 1;
     }
     
+    // >> Mesh loading & preprocessing <<
+
     MyMesh m;
 
     int loadMask;
@@ -213,17 +204,26 @@ int main(int argc, char* argv[])
     vcg::tri::UpdateTopology<MyMesh>::VertexFace(m);
     vcg::tri::RequireFFAdjacency(m);
     vcg::tri::UpdateTopology<MyMesh>::FaceFace(m);
+    vcg::tri::RequirePerVertexFlags<MyMesh>(m);
+    vcg::tri::UpdateFlags<MyMesh>::VertexBorderFromFaceAdj(m);
     
     vcg::tri::RequirePerFaceNormal(m);
+
+    // >> Gradient <<
 
     MyMesh::PerVertexAttributeHandle<vcg::Point3d> energyGrad = vcg::tri::Allocator<MyMesh>::GetPerVertexAttribute<vcg::Point3d>(m, "Combinatorial energy gradient");
     double totEnergy;
 
-    // perform standard gradient descent with argv[2] steps and a stepsize of argv[3]
+    double dt;
+    std::vector<double> dts;
+    clock_t clockStart;
+
+    // perform simple gradient descent with argv[2] steps and a stepsize of argv[3]
     int nSteps = atoi(argv[2]);
     float stepSize = atof(argv[3]);
     for(int step = 0; step < nSteps; step++)
     {
+        clockStart = clock();
         vcg::tri::UpdateNormal<MyMesh>::PerFaceNormalized(m);
         totEnergy = 0.0;
 
@@ -235,17 +235,25 @@ int main(int argc, char* argv[])
             totEnergy += energy;
         });
 
-        std::cout << "Step #" << step << "\tEnergy=" << totEnergy << std::endl;
-
         for(MyMesh::VertexIterator vIter = m.vert.begin(); vIter != m.vert.end(); vIter++)
-            (*vIter).P() += (energyGrad[vIter] * stepSize);
+            (*vIter).P() -= (energyGrad[vIter] * stepSize);
+
+        dt = ((double)(clock() - clockStart) / CLOCKS_PER_SEC);
+        std::cout << "Step #" << step << ": Energy=" << totEnergy << "\tTime=" << dt << std::endl;
+        dts.push_back(dt);    
     }
+
+    dt = 0;
+    for(double currDt : dts)
+        dt += currDt;
+    dt /= dts.size();
+    std::cout << "Mean time per step: " << dt << std::endl;
 
     totEnergy = 0.0;
     for(MyMesh::VertexIterator vIter = m.vert.begin(); vIter != m.vert.end(); vIter++)
         totEnergy += localCombinatorialEnergy(&*vIter);
 
-    std::cout << "Final Energy= " << totEnergy << std::endl;
+    std::cout << "Final energy: " << totEnergy << std::endl;
 
     vcg::tri::io::ExporterPLY<MyMesh>::Save(m, "out.ply", TriMask::IOM_NONE, false);
 
