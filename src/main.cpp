f01f6e78cc55ae4ba909e1d7403d7bf933590b1e
Luca Franceschi <l.franceschi5@studenti.unipi.it>
2022-07-16 12:38:14 +0200

    edge collapsing & post-processing refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,11 @@ using TriMask = vcg::tri::io::Mask;
 #include "mesh_matrix.hpp"
 #include "energy.hpp"
 #include "energy_grad.hpp"
+#include "remeshing.hpp"
 
 #include <time.h>
 
-#define MIN_ANGLE 0.4
+#define POSTPROCESSING_ANGLE_THRESHOLD 0.3
 
 
 // -------------------------------------------------------------------------------------------------
@@ -67,14 +68,11 @@ int main(int argc, char* argv[])
     float stepSize = atof(argv[3]);
     double totEnergy;
 
+    MeshPostProcessing<MyMesh> postProcessing(true, true, POSTPROCESSING_ANGLE_THRESHOLD);
+
     double dt;
     std::vector<double> dts;
     clock_t clockStart;
-
-    MyMesh::FaceIterator fIter;
-    double Ai, Aj, Ak;
-    int edgeToFlip;
-    bool edgeFlipped;
 
     for(int step = 0; step < nSteps; step++)
     {
@@ -91,34 +89,9 @@ int main(int argc, char* argv[])
         for(size_t v = 0; v < V.rows(); v++)
             m.vert[v].P() = vcg::Point3d(V(v, 0), V(v, 1), V(v, 2));
         
-        edgeFlipped = false;
-        for(fIter = m.face.begin(); fIter != m.face.end(); fIter++)
-        {
-            Ai = vcg::face::WedgeAngleRad<MyFace>(*fIter, 0);
-            Aj = vcg::face::WedgeAngleRad<MyFace>(*fIter, 1);
-            Ak = vcg::face::WedgeAngleRad<MyFace>(*fIter, 2);
-
-            edgeToFlip = -1;
-            if(Ai + Aj < MIN_ANGLE)
-                edgeToFlip = 0;
-            else if(Aj + Ak < MIN_ANGLE)
-                edgeToFlip = 1;
-            else if(Ak + Ai < MIN_ANGLE)
-                edgeToFlip = 2;
-
-            if(edgeToFlip >= 0 && vcg::face::CheckFlipEdge(*fIter, edgeToFlip))
-            {
-                vcg::face::FlipEdge<MyFace>(*fIter, edgeToFlip);
-                edgeFlipped = true;
-            }
-        }
-
-        if(edgeFlipped)
+        if(postProcessing.process(m))
         {
             std::cout << "Mesh topology has been altered" << std::endl;
-            vcg::tri::UpdateTopology<MyMesh>::VertexFace(m);
-            vcg::tri::UpdateTopology<MyMesh>::FaceFace(m);
-            vcg::tri::UpdateFlags<MyMesh>::VertexBorderFromFaceAdj(m);
             getMeshVF(m, V, F);
             getMeshStars(m, S);
             getMeshBorders(m, B);
---
+++ src/remeshing.cpp
@@ -0,0 +1,92 @@
+#include "remeshing.hpp"
+
+#include <vcg/simplex/face/topology.h>
+#include <vcg/complex/allocate.h>
+#include <vcg/complex/algorithms/update/topology.h>
+#include <vcg/complex/algorithms/update/flag.h>
+
+#include "mesh.hpp"
+
+
+template <class MeshType>
+MeshPostProcessing<MeshType>::MeshPostProcessing(bool doEdgeFlipping,
+                                                 bool doEdgeCollapsing,
+                                                 double angleThreshold) :
+    doEdgeFlipping(doEdgeFlipping),
+    doEdgeCollapsing(doEdgeCollapsing),
+    angleThreshold(angleThreshold)
+{}
+
+template MeshPostProcessing<MyMesh>::MeshPostProcessing(bool doEdgeFlipping,
+                                                        bool doEdgeCollapsing,
+                                                        double angleThreshold);
+
+
+template <class MeshType>
+bool MeshPostProcessing<MeshType>::process(MeshType& m)
+{
+    typedef typename MeshType::FaceType FaceType;
+    typedef typename MeshType::FaceIterator FaceIterator;
+    
+    FaceIterator fIter;
+    double faceAngles[3];
+    int edgeToFlip;
+    int edgeToCollapse;
+    bool meshModified = false;
+    
+    for(fIter = m.face.begin(); fIter != m.face.end(); fIter++)
+    {
+        if(fIter->IsD())
+            continue;
+        
+        faceAngles[0] = vcg::face::WedgeAngleRad<FaceType>(*fIter, 0);
+        faceAngles[1] = vcg::face::WedgeAngleRad<FaceType>(*fIter, 1);
+        faceAngles[2] = vcg::face::WedgeAngleRad<FaceType>(*fIter, 2);
+
+        edgeToFlip = -1;
+        edgeToCollapse = -1;
+        for(int i = 0; i < 2; i++)
+            if(faceAngles[i] < angleThreshold)
+            {
+                if(faceAngles[(i+1)%3] < angleThreshold)
+                {
+                    edgeToFlip = i;
+                    break;
+                }
+                else if(faceAngles[(i-1)%3] < angleThreshold)
+                {
+                    edgeToFlip = (i-1)%3;
+                    break;
+                }
+                else
+                {
+                    edgeToCollapse = (i+1)%3;
+                    break;
+                }
+            }
+
+        if(edgeToFlip >= 0 && vcg::face::CheckFlipEdge(*fIter, edgeToFlip))
+        {
+            vcg::face::FlipEdge<FaceType>(*fIter, edgeToFlip);
+            meshModified = true;
+        }
+        else if(edgeToCollapse >= 0 && vcg::face::FFLinkCondition(*fIter, edgeToCollapse))
+        {
+            vcg::face::FFEdgeCollapse<MeshType>(m, *fIter, edgeToCollapse);
+            meshModified = true;
+        }
+    }
+
+    if(meshModified)
+    {
+        vcg::tri::Allocator<MeshType>::CompactFaceVector(m);
+        vcg::tri::Allocator<MeshType>::CompactVertexVector(m);
+        vcg::tri::UpdateTopology<MeshType>::VertexFace(m);
+        vcg::tri::UpdateTopology<MeshType>::FaceFace(m);
+        vcg::tri::UpdateFlags<MeshType>::VertexBorderFromFaceAdj(m);
+    }
+
+    return meshModified;
+}
+
+template bool MeshPostProcessing<MyMesh>::process(MyMesh& m);
